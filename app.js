480dd3177fbeb40ed4da251c7a0ab08e439e68a6
DuanLeeWatkins <leeduan19@gmail.com>
2021-05-13 20:50:11 -0400

    add files

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require("express");
 const mongoose = require("mongoose");
-const passport = require('passport')
-const connectDB = require('./config/database')
+const passport = require("passport");
+const connectDB = require("./config/database");
 const logger = require("morgan");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
-const mainRoutes = require('./routes/main')
+const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
+const PORT = 8000;
+connectDB();
 
-connectDB()
-
-const app = express()
+const app = express();
 
 // Passport config
 require("./config/passport")(passport);
@@ -39,25 +39,19 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.use(flash());
 
-app.use("/", mainRoutes); // this is telling the app to use the router file to respond to requests made to the site and setting the landing page to our login view file 
+app.use("/", mainRoutes); // this is telling the app to use the router file to respond to requests made to the site and setting the landing page to our login view file
 app.use("/post", postRoutes);
 
-
-
-
-
 //Server Running
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
